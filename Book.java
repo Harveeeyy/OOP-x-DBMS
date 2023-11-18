84ecc2afdfb39a187ceb17d9c7c5bb691bec5ddc
Harvey Madera <130563262+Harveeeyy@users.noreply.github.com>
2023-11-18 19:11:52 +0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,46 @@
+package src;
+
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private int quantity;
+    private boolean available = true;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
