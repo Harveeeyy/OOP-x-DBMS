1b1eaf44e02f8375a2ce748c2a6bdc1293ec85fe
Harvey Madera <130563262+Harveeeyy@users.noreply.github.com>
2023-12-04 14:19:41 +0800

    Update Book.java final

--- Book.java
+++ Book.java
@@ -1,31 +1,17 @@
 package src;
 
-public class Book {
-    private String title;
-    private String author;
+public class Book extends LibraryItem {
     private String genre;
     private int quantity;
     private boolean available = true;
 
+    public Book(String title, String author, String genre) {
+        super(title, author, "Book");
+        this.genre = genre;
+    }
+
     public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
+        super(title, author, "Book");
     }
 
     public String getGenre() {
@@ -42,5 +28,30 @@ public class Book {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public void displayItem() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Book Genre: " + genre);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Available: " + available);
     }
 }
